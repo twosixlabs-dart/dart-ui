875e856ef2c174c4cc74f51b4d834d781c7f6c14
johnhungerford <hungerfordjustice@gmail.com>
2022-03-23 19:47:46 -0400

    Legacy peer deps in components/ npm install

--- scripts/build-js.sh
+++ scripts/build-js.sh
@@ -8,7 +8,7 @@ npm install --legacy-peer-deps || exit 1
 npm run package || exit 1
 
 cd ../components || exit 1
-npm install || exit 1
+npm install --legacy-peer-deps || exit 1
 npm run package || exit 1
 
 cd ../app-js || exit 1
