a5158ef6e82e682e07bd761c270dcd02948d0174
johnhungerford <hungerfordjustice@gmail.com>
2022-03-23 19:52:03 -0400

    Legacy peer deps in app-js/ npm install

--- scripts/build-js.sh
+++ scripts/build-js.sh
@@ -12,5 +12,5 @@ npm install --legacy-peer-deps || exit 1
 npm run package || exit 1
 
 cd ../app-js || exit 1
-npm install || exit 1
+npm install --legacy-peer-deps || exit 1
 npm run package || exit 1
