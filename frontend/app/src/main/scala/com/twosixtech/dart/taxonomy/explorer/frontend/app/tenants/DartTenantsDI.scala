8a51bb1888f36480f4402bb9ad666476d19ed4e4
johnhungerford <hungerfordjustice@gmail.com>
2022-04-07 12:25:29 -0400

    Added xhr calls to dart tenants component

--- frontend/app/src/main/scala/com/twosixtech/dart/taxonomy/explorer/frontend/app/tenants/DartTenantsDI.scala
+++ frontend/app/src/main/scala/com/twosixtech/dart/taxonomy/explorer/frontend/app/tenants/DartTenantsDI.scala
@@ -1,15 +1,20 @@
 package com.twosixtech.dart.taxonomy.explorer.frontend.app.tenants
 
 import com.twosixlabs.dart.auth.tenant.{ CorpusTenant, DartTenant, GlobalCorpus }
+import com.twosixtech.dart.scalajs.backend.{ HttpMethod, HttpRequest, HttpResponse }
 import com.twosixtech.dart.taxonomy.explorer.frontend.base.DartComponentDI
 import com.twosixtech.dart.taxonomy.explorer.frontend.base.context.DartContextDeps
+import com.twosixtech.dart.taxonomy.explorer.frontend.configuration.DartConfigDeps
 import japgolly.scalajs.react.Callback
 import japgolly.scalajs.react.vdom.VdomElement
+
+import scala.util.{ Failure, Success }
 
 trait DartTenantsDI {
 	this : DartComponentDI
 	  with DartTenantsLayoutDeps
-	  with DartContextDeps =>
+	  with DartContextDeps
+	  with DartConfigDeps =>
 
 	class DartTenants
 	  extends SimpleDartComponent[
@@ -23,14 +28,44 @@ trait DartTenantsDI {
 			renderProps : DartTenantsRenderContext,
 			context : DartContext
 		) : VdomElement = {
+			val client = context.backendContext.authClient
+
+			import scalajs.concurrent.JSExecutionContext.Implicits.queue
+
 			val addTenant : String => Callback = {
 				( tenantName : String ) =>
-					???
+					Callback( client.submit(
+						method = HttpMethod.Post,
+						request = HttpRequest(
+							url = dartConfig.tenantsBaseUrl + "/" + tenantName,
+						),
+					).onComplete {
+						case Success( HttpResponse( _, 201, _ ) ) => context.refreshTenants.runNow()
+						case Success( HttpResponse( _, status, body ) ) =>
+							context.report.logMessage( "Unable to add tenant", s"status: $status, body: ${body.toString}" )
+							  .>>( context.refreshTenants ).runNow()
+						case Failure( e ) =>
+							context.report.logMessage( "Unable to add tenant", e )
+							  .>>( context.refreshTenants ).runNow()
+					} )
 			}
 
 			val removeTenant : String => Callback = {
 				( tenantName : String ) =>
-					???
+					Callback( client.submit(
+						method = HttpMethod.Delete,
+						request = HttpRequest(
+							url = dartConfig.tenantsBaseUrl + "/" + tenantName,
+						),
+					).onComplete {
+						case Success( HttpResponse( _, 200, _ ) ) => context.refreshTenants.runNow()
+						case Success( HttpResponse( _, status, body ) ) =>
+							context.report.logMessage( "Unable to remove tenant", s"status: $status, body: ${body.toString}" )
+							  .>>( context.refreshTenants ).runNow()
+						case Failure( e ) =>
+							context.report.logMessage( "Unable to remove tenant", e )
+							  .>>( context.refreshTenants ).runNow()
+					} )
 			}
 
 			dartTenantsLayout( DartTenants.LayoutProps(
