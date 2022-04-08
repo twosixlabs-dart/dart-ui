97aab317044f4e1f58f9de3202c5b646fed315e9
johnhungerford <hungerfordjustice@gmail.com>
2022-04-08 16:14:53 -0400

    Used some standard component styling (need to add spacing, wrap, title, and center)

--- frontend/app/src/main/scala/com/twosixtech/dart/taxonomy/explorer/frontend/app/tenants/layouts/GenericDartTenantsLayoutDI.scala
+++ frontend/app/src/main/scala/com/twosixtech/dart/taxonomy/explorer/frontend/app/tenants/layouts/GenericDartTenantsLayoutDI.scala
@@ -1,13 +1,20 @@
 package com.twosixtech.dart.taxonomy.explorer.frontend.app.tenants.layouts
 
 import com.twosixlabs.dart.auth.tenant.DartTenant
+import com.twosixtech.dart.scalajs.layout.button.HoverButton
+import com.twosixtech.dart.scalajs.layout.button.iconbutton.IconButton
+import com.twosixtech.dart.scalajs.layout.button.iconbutton.mui.IconButtonMui
 import com.twosixtech.dart.scalajs.layout.button.regular.Button
 import com.twosixtech.dart.scalajs.layout.button.regular.mui.ButtonMui
 import com.twosixtech.dart.scalajs.layout.form.textinput.{ TextInput, TextInputMui }
+import com.twosixtech.dart.scalajs.layout.icon.Icons.SyncIconMui
 import com.twosixtech.dart.scalajs.layout.text.{ Text, TextMui }
+import com.twosixtech.dart.scalajs.layout.types
+import com.twosixtech.dart.taxonomy.explorer.frontend.app.explorer.state.access.layouts.GenericStateAccessComponentLayoutClasses.refreshButtonClass
 import com.twosixtech.dart.taxonomy.explorer.frontend.app.tenants.{ DartTenantsDI, DartTenantsLayoutDeps }
 import com.twosixtech.dart.taxonomy.explorer.frontend.base.DartComponentDI
 import com.twosixtech.dart.taxonomy.explorer.frontend.base.context.DartContextDeps
+import japgolly.scalajs.react.Callback
 import japgolly.scalajs.react.vdom.VdomElement
 import japgolly.scalajs.react.vdom.html_<^._
 
@@ -21,6 +28,17 @@ trait GenericDartTenantsLayoutDI
 	override type DartTenantsLayoutState = Option[ String ]
 
 	override val dartTenantsLayout : DartTenantsLayout = GenericDartTenantsLayout
+
+	import scalacss.DevDefaults._
+
+	object Styles extends StyleSheet.Inline {
+
+		import dsl._
+
+		val invisible = style( visibility.hidden )
+	}
+
+	Styles.addToDocument()
 
 	object GenericDartTenantsLayout extends DartTenantsLayout {
 		override def render(
@@ -37,11 +55,11 @@ trait GenericDartTenantsLayoutDI
 			import com.twosixtech.dart.scalajs.layout.styles.ClassNameConversions._
 
 			<.div(
-				ButtonMui(
+				IconButtonMui( IconButton.Props(
 					onClick = props.refreshTenants,
-					element = "Refresh",
-					classes = Button.Classes( refreshButtonClass.cName ),
-				),
+					icon = SyncIconMui(),
+					classes = IconButton.Classes( refreshButtonClass.cName ),
+				) ),
 				<.div(
 					tenantsListClass.cName,
 					props.tenants.map( v => <.div(
@@ -51,33 +69,58 @@ trait GenericDartTenantsLayoutDI
 							v,
 							classes = Text.Classes( tenantNameClass.cName ),
 						),
-						if ( v == DartTenant.globalId ) EmptyVdom
-						else ButtonMui(
-							onClick = props.removeTenant( v ),
-							element = "Remove",
-							classes = Button.Classes( removeTenantButtonClass.cName ),
+						if ( v == DartTenant.globalId ) HoverButton(
+							left = true,
+							remove = true,
+							onClick = Some( props.removeTenant( v ) ),
+							element = TextMui(
+								element = v,
+								classes = Text.Classes( tenantNameClass.cName )
+							),
+							classes = HoverButton.Classes(
+								button = removeTenantButtonClass.cName,
+							)
+
+						) else HoverButton(
+							left = true,
+							remove = true,
+							onClick = None,
+							element = TextMui(
+								element = v,
+								classes = Text.Classes( tenantNameClass.cName )
+							),
+							classes = HoverButton.Classes(
+								button = Styles.invisible.cName,
+							),
 						),
 					) ).toVdomArray,
 				),
 				state match {
 					case None =>
 						ButtonMui(
+							element = "+",
+							size = Button.Small,
+							style = Button.Text,
 							onClick = scope.setState( Some( "" ) ),
-							element = "Add",
 							classes = Button.Classes( addTenantButtonClass.cName )
 						)
 					case Some( tenantId ) =>
 						<.div(
-							TextInputMui( TextInput.Props(
-								value = Some( tenantId ),
-								onChange = Some( newId => scope.setState( Some( newId ) ) ),
-								onEnter = Some( props.addTenant( tenantId ) >> scope.setState( None ) ),
-								classes = TextInput.Classes( input = newTenantInputClass.cName )
-							) ),
-							ButtonMui(
-								onClick = props.addTenant( tenantId ) >> scope.setState( None ),
-								element = "Add",
-								classes = Button.Classes( addTenantInputButtonClass.cName )
+							HoverButton(
+								onClick = Some( props.addTenant( tenantId ) >> scope.setState( None ) ),
+								element = TextInputMui( TextInput.Props(
+									value = Some( tenantId ),
+									autoFocus = true,
+									onChange = Some( newId => scope.setState( Some( newId ) ) ),
+									onEnter = Some( props.addTenant( tenantId ) >> scope.setState( None ) ),
+									onEscape = Some( scope.setState( None ) ),
+									variant = TextInput.Outlined,
+									size = types.Small,
+									classes = TextInput.Classes( input = newTenantInputClass.cName )
+								) ),
+								classes = HoverButton.Classes(
+									button = addTenantInputButtonClass.cName,
+								),
 							),
 						)
 				},
