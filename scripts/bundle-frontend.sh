a9db680e6e96cedd127ea486ae2e26ad966a77bd
johnhungerford <hungerfordjustice@gmail.com>
2022-05-11 00:14:01 -0400

    Lazy load components (attempt 2) (#10)
    * Lazy loading scala-2.13 components
    
    * Building frontend from app-js webpack config
    
    * Working with split modules
    
    * Added scalajs-bundler to frontend/app for testing
    
    * Fixed sbt command in build action
    
    * Fixed scalajs-bundler config in build.sbt
    
    * Added buildFrontendDeps tasks and divided assembleJs and runApp tasks into Full and Main
    
    * Added dart-ui-components and dart-ui-scala13-components to frontend app npm deps
    
    * Logging js artifacts
    
    * .gitignoring app-js/src/main

--- scripts/bundle-frontend.sh
+++ scripts/bundle-frontend.sh
@@ -4,12 +4,16 @@
 ## built using `build-raw-js.sh`
 
 rm frontend/app-js/src/main/*
+echo "Copying scala.js artifacts to app-js"
 if [ $1="prod" ]; then
   # for prod, only move js over (no maps)
+  ls frontend/app/target/scala-2.12/app-opt/
   cp frontend/app/target/scala-2.12/app-opt/*.js frontend/app-js/src/main/
 else
+  ls frontend/app/target/scala-2.12/app-fastopt/
   cp frontend/app/target/scala-2.12/app-fastopt/* frontend/app-js/src/main/
 fi
+ls frontend/app-js/src/main/
 
 cd frontend/app-js || exit 1
 npm install --legacy-peer-deps || exit 1
